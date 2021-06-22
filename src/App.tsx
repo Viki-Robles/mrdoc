a328d6c20e3052f0a57a4d1370e1c7ad3b252763
Vicky V <vasiliki.robles@gmail.com>
2021-06-22 14:49:28 +0100

    feat: Fixing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { SIGN_UP_PAGE, SIGN_IN_PAGE, DASHBOARD_PAGE } from "./paths/paths";
 import NavBar from "./components/NavBar/NavBar";
 import SignUp from "./components/SignUp/SignUp";
 import SignIn from "./components/SignIn/SignIn";
 import { Dashboard } from "./components/Dashboard/Dashboard";
 import { AuthProvider } from "./providers/AuthProvider";
+import {
+  SIGN_IN_PAGE_PATH,
+  SIGN_UP_PAGE_PATH,
+  DASHBOARD_PAGE_PATH,
+} from "./config/paths";
 
 function App(): JSX.Element {
   return (
@@ -14,9 +18,9 @@ function App(): JSX.Element {
       <Router>
         <AuthProvider>
           <Switch>
-            <Route path={SIGN_UP_PAGE} component={SignUp} />
-            <Route path={SIGN_IN_PAGE} component={SignIn} />
-            <Route path={DASHBOARD_PAGE} component={Dashboard} />
+            <Route path={SIGN_UP_PAGE_PATH} component={SignUp} />
+            <Route path={SIGN_IN_PAGE_PATH} component={SignIn} />
+            <Route path={DASHBOARD_PAGE_PATH} component={Dashboard} />
           </Switch>
         </AuthProvider>
       </Router>
--- src/config/paths.ts
+++ src/config/paths.ts
@@ -1,4 +1,10 @@
+export const BASE_URL_PATH = "/";
+
 export const HOME_PAGE_PATH = "/";
-export const SIGN_IN_PAGE_PATH = "/signIn";
+
+export const SIGN_IN_PAGE_PATH = "/sign-in";
+export const SIGN_UP_PAGE_PATH = "/sign-up";
+
+export const DASHBOARD_PAGE_PATH = "/dashboard";
 export const FORGOT_PASSWORD_PAGE_PATH = "/forgot-password";
 export const RESET_PASSWORD_PAGE_PATH = "/reset-password";
