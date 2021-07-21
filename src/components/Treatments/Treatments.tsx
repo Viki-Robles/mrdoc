4f800d03843e1c55a249492da6e37fb662fc0737
Vicky <vasiliki.robles@gmail.com>
2021-07-21 10:53:45 +0100

    feat: add mutation query

--- src/components/Treatments/Treatments.tsx
+++ src/components/Treatments/Treatments.tsx
@@ -1,7 +1,8 @@
 import React, { Fragment, useState } from "react";
 import { Box, Grid, Typography, Button } from "@material-ui/core";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql, useLazyQuery, useMutation } from "@apollo/client";
 import { Treatment } from "../Treatment/Treatment";
+// import { DENTIST_CATEGORY } from "../../utils/graphql/clinics";
 
 export const GET_TREATMENTS = gql`
   query {
@@ -26,35 +27,52 @@ export const GET_CATEGORY_DENTIST = gql`
     }
   }
 `;
+
+export const DENTIST_CATEGORY = gql`
+  mutation {
+    update_clinics(
+      where: { category: { _eq: "Dentist" } }
+      _set: { category: "Dentist" }
+    ) {
+      affected_rows
+      returning {
+        category
+        location
+        id
+        name
+      }
+    }
+  }
+`;
 export interface TreatmentsProps {
   location: string;
 }
 
 export const Treatments = (): JSX.Element => {
-  const { data, loading, error } = useQuery(GET_TREATMENTS);
-  const [showCategory, setShowCategory] = useState<boolean>(false);
+  const [getClinics, { data, loading, error }] = useMutation(DENTIST_CATEGORY);
 
   if (loading) return <p>Loading...</p>;
   if (error) console.log(error);
 
   return (
     <Fragment>
-      <Button onClick={() => setShowCategory(!showCategory)}>Dentist</Button>
-
-      <Box display="flex" flexWrap="wrap" justifyContent="space-around" m={2}>
-        {data?.clinics?.map(({ name, id, price, location, subcategory }) => {
-          return (
-            <Treatment
-              name={name}
-              id={id}
-              price={price}
-              location={location}
-              key={id}
-              subcategory={subcategory}
-            />
-          );
-        })}
-      </Box>
+      <Button onClick={() => getClinics()}>Dentist</Button>
+      {data && (
+        <Box display="flex" flexWrap="wrap" justifyContent="space-around" m={2}>
+          {data?.clinics?.map(({ name, id, price, location, subcategory }) => {
+            return (
+              <Treatment
+                name={name}
+                id={id}
+                price={price}
+                location={location}
+                key={id}
+                subcategory={subcategory}
+              />
+            );
+          })}
+        </Box>
+      )}
     </Fragment>
   );
 };
--- src/types/treatment.ts
+++ src/types/treatment.ts
@@ -1,1 +1,8 @@
-export interface Treatment {}
+export interface Treatment {
+  name: string;
+  id: number;
+  category: string | null;
+  location: string | null;
+  subcategory: string | null;
+  price: number;
+}
