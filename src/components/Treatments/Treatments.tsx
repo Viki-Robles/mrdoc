9e83e46312f9e1695ad8ec8ee4c5ff19364fe755
Viki Robles <vasiliki.robles@gmail.com>
2021-07-27 11:43:11 +0100

    feat: add toggles

--- src/components/Treatments/Treatments.tsx
+++ src/components/Treatments/Treatments.tsx
@@ -1,28 +1,27 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { Box, Button } from "@material-ui/core";
 import { Treatment } from "../Treatment/Treatment";
 import { useFetchData } from "../../hooks/useFetchData/useFetchData";
 
 export const Treatments = (): JSX.Element => {
-  const [showDental, setShowDental] = useState<boolean>(false);
-  const [showEyeCare, setShowEyeCare] = useState<boolean>(false);
+  const [showDental, setShowDental] = useState<boolean>(true);
+  const [showEyeCare, setShowEyeCare] = useState<boolean>(true);
   const { dentistData, eyeCareData } = useFetchData();
 
-  // const switchResult = () => {
-  //   if (!showDental) {
-  //     setShowDental(!showDental);
-  //     setShowEyeCare(showEyeCare);
-  //   }
-  //   if (!showEyeCare) {
-  //     setShowEyeCare(!showEyeCare);
-  //     setShowDental(showDental);
-  //   }
-  // };
+  const toggleDental = () => {
+    setShowDental(showDental);
+    setShowEyeCare(!showEyeCare);
+  };
+
+  const toggleEyeCare = () => {
+    setShowDental(!showDental);
+    setShowEyeCare(showEyeCare);
+  };
 
   return (
     <Fragment>
-      <Button onClick={() => setShowDental(!showDental)}>Dentist</Button>
-      <Button onClick={() => setShowEyeCare(!showEyeCare)}>Eye Care</Button>
+      <Button onClick={toggleDental}>Dentist</Button>
+      <Button onClick={toggleEyeCare}>Eye Care</Button>
       {showDental && (
         <Box display="flex" flexWrap="wrap" justifyContent="space-around" m={2}>
           {dentistData?.map(({ name, price, location, category, id }) => {
