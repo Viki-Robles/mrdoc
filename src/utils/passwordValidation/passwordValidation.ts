a497f7f9234e041ffc816ee92494ee03630d6ee5
Viki Robles <vasiliki.robles@gmail.com>
2021-09-26 21:19:09 +0100

    feat: fix tests

--- src/utils/passwordValidation/passwordValidation.ts
+++ src/utils/passwordValidation/passwordValidation.ts
@@ -1,20 +1,20 @@
-import * as Yup from "yup";
+import * as Yup from 'yup'
 
 export const passwordValidation = Yup.string()
   .trim()
-  .required("Required")
+  .required('Required')
   .matches(
     /(?=.*[A-Z])/,
-    "Your password needs to have at least one capital letter"
+    'Your password needs to have at least one capital letter',
   )
   .matches(
     /(?=.*[!@#$%^&*])/,
-    "Your password needs to have at least one special character"
+    'Your password needs to have at least one special character',
   )
   .matches(
     /(?=.*[a-z])/,
-    "Your password needs to have at least one lower case character"
+    'Your password needs to have at least one lower case character',
   )
-  .matches(/(?=.*[0-9])/, "Your password needs to have at least one number")
+  .matches(/(?=.*[0-9])/, 'Your password needs to have at least one number')
   .min(8)
-  .max(200);
+  .max(200)
