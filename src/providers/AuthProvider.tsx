3fc903ececd6dc2ab98486c0b853d5fdc66e1fdf
Vicky Vasilopoulou <vickyvasilopoulou@Vickys-MacBook.local>
2022-02-19 16:09:27 +0000

    feat: create firebase user with display name

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -62,40 +62,19 @@ export function useAuth(): AuthContextModel {
 export const AuthProvider = ({ children }: AuthProviderProps): JSX.Element => {
   const [user, setUser] = useState<User | null | undefined>()
 
-  async function signUp(
+  const signUp = async (
     displayName: string,
     email: string,
     password: string,
-  ): Promise<void> {
+  ): Promise<void> => {
     try {
-      // cant use admin cause this package is only on the backend of an application
-      // TODOS:
-      // A user has been created but is not 100% correct as the displayName is not passed.
-      // check docs in here: https://firebase.google.com/docs/auth/admin/manage-users
-      // the previous commit: https://github.com/Viki-Robles/mrdoc/commit/03a51497ad511f99928c2b62288750bd5ce9ebe0 creates
-      // user on firestore on the console.
-      // check also this to help you out https://stackoverflow.com/questions/46727150/create-user-with-firebase-admin-sdk-that-can-signin-using-email-and-password
-
-      // const res = await createUserWithEmailAndPassword({
-      //   displayName,
-      //   email,
-      //   password,
-      // })
-      // const userName = res.displayName
-      // const userEmail = res.email
-      // const userPassword = res.passwordHash
-      // const userUid = res.uid
-      // uid: userUid,
-      // displayName: userName,
-      // email: userEmail,
-      // password: userPassword,
       const res = await createUserWithEmailAndPassword(auth, email, password)
       const user = res.user
       await addDoc(collection(db, 'users'), {
         uid: user?.uid,
-        displayName: user?.displayName,
+        displayName,
+        authProvider: 'local',
         email: user?.email,
-        // password: user.,
       })
     } catch (err) {
       console.error(err)
