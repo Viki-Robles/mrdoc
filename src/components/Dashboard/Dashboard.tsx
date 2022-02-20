abc6820ebe964608e962aba80ec9af84d8ba8a7b
VickyRobles <vasiliki.robles@gmail.com>
2022-02-20 11:47:23 +0000

    feat: remove functions folder

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
-// import { useAuthState } from 'react-firebase/hooks/auth'
 import { useFetchDoctors } from '../../hooks/useFetchDoctors/useFetchDoctors'
 import { DoctorsGalleryItems } from '../DoctorsGalleryItems/DoctorsGalleryItems'
 import { FavouriteProvider } from '../Favourite/FavouriteProvider'
 import { UserProfile } from '../UserProfile/UserProfile'
 import { Box } from 'theme-ui'
 import { getAuth } from '@firebase/auth'
+import { db } from '../../config/firebase'
 
 const Dashboard = (doctor_id: string): JSX.Element => {
   const { doctorsData } = useFetchDoctors()
   const { currentUser } = getAuth()
+
+  // const getUsersDisplayName = (next, fallback) => {
+  //   const user = getAuth().onAuthStateChanged((authUser) => {
+  //     if (authUser) {
+  //       const dbUser = db.coll
+  //     }
+  //   })
+  // }
 
   return (
     <FavouriteProvider doctor_id={doctor_id}>
@@ -25,3 +33,32 @@ const Dashboard = (doctor_id: string): JSX.Element => {
 }
 
 export default Dashboard
+
+// class Firebase {
+//   constructor() {
+//     app.initializeApp(config).firestore();
+//     /* helpers */
+//     this.fieldValue = app.firestore.FieldValue;
+
+//     /* Firebase APIs */
+//     this.auth = app.auth();
+//     this.db = app.firestore();
+
+// onAuthUserListener = (next, fallback) =>
+//     this.auth.onAuthStateChanged(authUser => {
+//       if (authUser) {
+//            this.db.collection('users').doc(authUser.uid)
+//               .get()
+//               .then(snapshot => {
+//                 const userData = snapshot.data();
+//                 console.log(userData);
+//                 //Do whatever you need with userData
+//                 //i.e. merging it with authUser
+//                 //......
+
+//                 next(authUser);
+//           });
+//       } else {
+//         fallback();
+//       }
+//     });
