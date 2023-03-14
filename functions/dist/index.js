a4524340d1c89338c57398fadbe1554296c72624
VickyRobles <vasiliki.robles@gmail.com>
2023-03-14 20:15:43 +0000

    fix: index file after hosting deployment failure

--- functions/dist/index.js
+++ functions/dist/index.js
@@ -5,30 +5,41 @@ var admin = require("firebase-admin");
 var cors = require("cors");
 admin.initializeApp(functions.config().firebase);
 var updateClaims = function (uid) {
-    return admin.auth().setCustomUserClaims(uid, {
-        "https://hasura.io/jwt/claims": {
-            "x-hasura-default-role": "user",
-            "x-hasura-allowed-roles": ["user"],
-            "x-hasura-user-id": uid,
-        },
-    });
+  return admin.auth().setCustomUserClaims(uid, {
+    "https://hasura.io/jwt/claims": {
+      "x-hasura-default-role": "user",
+      "x-hasura-allowed-roles": ["user"],
+      "x-hasura-user-id": uid,
+    },
+  });
 };
-exports.processSignUp = functions.auth
-    .user()
-    .onCreate(function (user) { return updateClaims(user.uid); });
+exports.processSignUp = functions.auth.user().onCreate(function (user) {
+  return updateClaims(user.uid);
+});
 exports.refreshToken = functions.https.onRequest(function (req) {
+  var _a;
+  console.log(
+    "TOKEN REFRESH",
+    (_a = req === null || req === void 0 ? void 0 : req.query) === null ||
+      _a === void 0
+      ? void 0
+      : _a.uid,
+  );
+  cors(function (req, res) {
     var _a;
-    console.log("TOKEN REFRESH", (_a = req === null || req === void 0 ? void 0 : req.query) === null || _a === void 0 ? void 0 : _a.uid);
-    cors(function (req, res) {
-        var _a;
-        updateClaims((_a = req === null || req === void 0 ? void 0 : req.query) === null || _a === void 0 ? void 0 : _a.uid)
-            .then(function () {
-            res.status(200).send("success");
-        })
-            .catch(function (error) {
-            console.error("REFRESH ERROR", error);
-            res.status(400).send(error);
-        });
-    });
+    updateClaims(
+      (_a = req === null || req === void 0 ? void 0 : req.query) === null ||
+        _a === void 0
+        ? void 0
+        : _a.uid,
+    )
+      .then(function () {
+        res.status(200).send("success");
+      })
+      .catch(function (error) {
+        console.error("REFRESH ERROR", error);
+        res.status(400).send(error);
+      });
+  });
 });
 //# sourceMappingURL=index.js.map
